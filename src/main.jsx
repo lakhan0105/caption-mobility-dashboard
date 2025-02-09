8b87d00c51410f201e5675b7fbb637e5f7e3de55
lakhan0105 <lakhan0105@gmail.com>
2025-02-09 18:26:17 +0530

    created the start page

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./store.js";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router";
+import { StartPage } from "./Pages/index.js";
+
+// router
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<StartPage />} />
+      <Route />
+    </>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router}></RouterProvider>
+    </Provider>
+  </StrictMode>
+);
